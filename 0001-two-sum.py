0be61f18e5ac8136e7fd4f54a1326c42c1822d0a
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-06 19:31:32 -0400

    Accepted (solution #2)

--- 0001-two-sum.py
+++ 0001-two-sum.py
@@ -11,12 +11,23 @@ class Solution:
         #         if i != j and m + n == target:
         #             return [i, j]
 
+        # d = {}
+        # for i, m in enumerate(nums):
+        #     for j, n in enumerate(nums):
+        #         if i != j and m + n not in d:
+        #             d[m + n] = [i, j]
+        # return d[target]
+
         d = {}
-        for i, m in enumerate(nums):
-            for j, n in enumerate(nums):
-                if i != j and m + n not in d:
-                    d[m + n] = [i, j]
-        return d[target]
+        for i, num in enumerate(nums):
+            d[num] = i
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in d:
+                j = d[diff]
+                if i != j:
+                    return [i, j]
 
 
 if __name__ == '__main__':

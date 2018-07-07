212ec95141def5120e0c179d6e4348a7c88af725
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-07 13:22:50 -0400

    Stub

---
+++ 0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+
+def main():
+    s = Solution()
+    print(s.lengthOfLongestSubstring("abcabcbb"))
+    print(s.lengthOfLongestSubstring("bbbbb"))
+    print(s.lengthOfLongestSubstring("pwwkew"))
+
+
+if __name__ == '__main__':
+    main()

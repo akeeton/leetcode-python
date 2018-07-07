825d34b564e59f00ba75380f2bcb92fa8dc37d4e
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-07 14:19:48 -0400

    Mostly working

--- 0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters.py
@@ -5,6 +5,31 @@ class Solution:
         :rtype: int
         """
 
+        # chars = list(s)
+        chars = s
+        max_len = 0
+        max_len_substring = ""
+        for left_index, start_char in enumerate(chars):
+            for right_index in reversed(range(left_index + 1, len(chars))):
+                substring = chars[left_index:right_index + 1]
+                char_counts = {}
+
+                for char in substring:
+                    char_counts[char] = 1 if char not in char_counts else char_counts[char] + 1
+
+                char_count_dup_gen = (char_count[1] > 1 for char_count in char_counts.items())
+
+                if not any(char_count_dup_gen) and len(substring) > max_len:
+                    max_len = len(substring)
+                    max_len_substring = substring
+
+                # print(substring)
+                # print(char_counts)
+                # print(list(char_count_dup_gen))
+
+        print("Max substring: '{}'".format(max_len_substring))
+        return max_len
+
 
 def main():
     s = Solution()

f06713f3eb485dc4b47a9e29430f2e8a30034504
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-08 13:33:48 -0400

    0003 927/983 tests passed - time limit exceeded

--- 0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from collections import deque
+import copy
 
 
 class Solution:
@@ -9,13 +11,13 @@ class Solution:
         """
 
         char_counts = Counter(s)
-        chars_minus_left = s[:]
+        chars_minus_left = deque(s[:])
         max_len = 0
 
         while len(chars_minus_left):
             char_counts_subtracted_from_right = Counter()
 
-            chars_minus_left_and_right = chars_minus_left[:]
+            chars_minus_left_and_right = copy.copy(chars_minus_left)
 
             while len(chars_minus_left_and_right) > max_len:
                 char_count_dup_gen = (char_count[1] > 1 for char_count in char_counts.items())
@@ -26,24 +28,24 @@ class Solution:
                     right_char = chars_minus_left_and_right[-1]
                     char_counts[right_char] -= 1
                     char_counts_subtracted_from_right[right_char] += 1
-                    chars_minus_left_and_right = chars_minus_left_and_right[:-1]
+                    chars_minus_left_and_right.pop()
 
             char_counts += char_counts_subtracted_from_right
 
             char_counts[chars_minus_left[0]] -= 1
-            chars_minus_left = chars_minus_left[1:]
+            chars_minus_left.popleft()
 
         return max_len
 
 
 def main():
     s = Solution()
-    # print(s.lengthOfLongestSubstring("abcabcbb"))
-    # print(s.lengthOfLongestSubstring("bbbbb"))
-    # print(s.lengthOfLongestSubstring("pwwkew"))
+    print(s.lengthOfLongestSubstring("abcabcbb"))
+    print(s.lengthOfLongestSubstring("bbbbb"))
+    print(s.lengthOfLongestSubstring("pwwkew"))
 
-    for i in range(1000):
-        s.lengthOfLongestSubstring("uygevhexbfvafrqzfikrstgjlenkuooqmwvhebhhgciovanaiztbszmffbrzpfscenlkqsrzwznrcctkbnnvoaduduvtanxgc")
+    # for i in range(1000):
+    #     s.lengthOfLongestSubstring("uygevhexbfvafrqzfikrstgjlenkuooqmwvhebhhgciovanaiztbszmffbrzpfscenlkqsrzwznrcctkbnnvoaduduvtanxgc")
 
     print("done")
 

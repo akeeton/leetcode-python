8d3a883f382aac2c2b4e2dbbd9acb5482b055f3f
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-08 13:53:08 -0400

    0003 982/983 tests passed - time limit exceeded

--- 0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from collections import deque
 import copy
+import yappi
 
 
 class Solution:
@@ -20,9 +21,13 @@ class Solution:
             chars_minus_left_and_right = copy.copy(chars_minus_left)
 
             while len(chars_minus_left_and_right) > max_len:
-                char_count_dup_gen = (char_count[1] > 1 for char_count in char_counts.items())
+                any_dups = False
+                for char_count in char_counts.items():
+                    if char_count[1] > 1:
+                        any_dups = True
+                        break
 
-                if not any(char_count_dup_gen):
+                if not any_dups:
                     max_len = len(chars_minus_left_and_right)
                 else:
                     right_char = chars_minus_left_and_right[-1]
@@ -40,15 +45,21 @@ class Solution:
 
 def main():
     s = Solution()
-    print(s.lengthOfLongestSubstring("abcabcbb"))
-    print(s.lengthOfLongestSubstring("bbbbb"))
-    print(s.lengthOfLongestSubstring("pwwkew"))
+    # print(s.lengthOfLongestSubstring("abcabcbb"))
+    # print(s.lengthOfLongestSubstring("bbbbb"))
+    # print(s.lengthOfLongestSubstring("pwwkew"))
 
-    # for i in range(1000):
-    #     s.lengthOfLongestSubstring("uygevhexbfvafrqzfikrstgjlenkuooqmwvhebhhgciovanaiztbszmffbrzpfscenlkqsrzwznrcctkbnnvoaduduvtanxgc")
+    for i in range(100):
+        s.lengthOfLongestSubstring("uygevhexbfvafrqzfikrstgjlenkuooqmwvhebhhgciovanaiztbszmffbrzpfscenlkqsrzwznrcctkbnnvoaduduvtanxgc")
 
     print("done")
 
 
 if __name__ == '__main__':
+    yappi.start()
+
     main()
+
+    columns = {0: ("name", 100), 1: ("ncall", 10), 2: ("tsub", 16), 3: ("ttot", 16), 4: ("tavg", 16)}
+    yappi.get_func_stats().print_all(columns=columns)
+    yappi.get_thread_stats().print_all()

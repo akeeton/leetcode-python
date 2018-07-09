403321f7f7a8f90a3bff4758ae6b9b6d75d5b74a
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-08 20:09:10 -0400

    0021 Accepted

--- 0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists.py
@@ -24,22 +24,59 @@ class ListNode:
 
         return s[:-4]
 
+
 class Solution:
-    def mergeTwoLists(self, l1, l2):
+    def mergeTwoLists(self, head_a, head_b):
         """
-        :type l1: ListNode
-        :type l2: ListNode
+        :type head_a: ListNode
+        :type head_b: ListNode
         :rtype: ListNode
         """
+
+        node_a = head_a
+        node_b = head_b
+        head_result = None  # type: ListNode
+        node_result = None  # type: ListNode
+
+        while True:
+            if node_a is None and node_b is None:
+                break
+            elif node_a is not None and node_b is None:
+                val_min = node_a.val
+                node_a = node_a.next
+            elif node_a is None and node_b is not None:
+                val_min = node_b.val
+                node_b = node_b.next
+            else:
+                if node_a.val <= node_b.val:
+                    val_min = node_a.val
+                    node_a = node_a.next
+                else:
+                    val_min = node_b.val
+                    node_b = node_b.next
+
+            node_new = ListNode(val_min)
+            if head_result is None:
+                head_result = node_new
+                node_result = head_result
+            else:
+                node_result.next = node_new
+                node_result = node_result.next
+
+            pass
+
+        return head_result
 
 
 def main():
     sol = Solution()
     list_a = ListNode.from_list([1, 2, 4])
     list_b = ListNode.from_list([1, 3, 4])
+    list_ab = sol.mergeTwoLists(list_a, list_b)
 
     print(list_a)
     print(list_b)
+    print(list_ab)
 
 
 if __name__ == '__main__':

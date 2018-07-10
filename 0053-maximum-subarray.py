d93f6854294dca0066e5213bdd64a373fd3cd6b0
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-10 07:43:17 -0400

    0053 Accepted - runtime 48 ms

--- 0053-maximum-subarray.py
+++ 0053-maximum-subarray.py
@@ -1,23 +1,20 @@
 class Solution:
-    @staticmethod
-    def max_sub_arrays_ending_with_index(nums):
-        max_sums = []
-
-        for index_end, num_at_end in enumerate(nums):
-            if index_end == 0:
-                max_sums.append(num_at_end)
-            else:
-                max_sums.append(max(num_at_end, num_at_end + max_sums[index_end - 1]))
-
-        return max_sums
-
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
 
-        return max(Solution.max_sub_arrays_ending_with_index(nums))
+        max_sum = None
+        max_sum_prev = None
+        for index_end, num_at_end in enumerate(nums):
+            if index_end == 0:
+                max_sum = max_sum_prev = num_at_end
+            else:
+                max_sum_prev = max(num_at_end, num_at_end + max_sum_prev)
+                max_sum = max(max_sum, max_sum_prev)
+
+        return max_sum
 
 
 def main():

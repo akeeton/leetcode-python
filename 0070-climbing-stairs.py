644f88021ca9df387a81bde01ddcaa9cc42c65b9
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-10 07:59:04 -0400

    0070 Accepted - runtime 36 ms

--- 0070-climbing-stairs.py
+++ 0070-climbing-stairs.py
@@ -1,22 +1,33 @@
 class Solution:
-    def climbStairs(self, n):
+    def climbStairs(self, num_steps):
         """
-        :type n: int
+        :type num_steps int
         :rtype: int
         """
 
-        if n == 1:
+        ways_to_climb_two_fewer_steps = 1
+        ways_to_climb_one_fewer_step = 2
+        ways_to_climb_n_steps = None
+
+        for n in range(3, num_steps + 1):
+            ways_to_climb_n_steps = ways_to_climb_two_fewer_steps + ways_to_climb_one_fewer_step
+
+            ways_to_climb_two_fewer_steps = ways_to_climb_one_fewer_step
+            ways_to_climb_one_fewer_step = ways_to_climb_n_steps
+
+        if num_steps == 1:
             return 1
-        elif n == 2:
+        elif num_steps == 2:
             return 2
         else:
-            return self.climbStairs(n - 2) + self.climbStairs(n - 1)
+            return ways_to_climb_n_steps
 
 
 def main():
     sol = Solution()
     print(sol.climbStairs(2))
     print(sol.climbStairs(3))
+    print(sol.climbStairs(35))
 
 
 if __name__ == '__main__':

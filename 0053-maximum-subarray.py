fc0258642f0b091e056f153338bdf2ceebbc1a7a
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-07-10 02:38:30 -0400

    0053 WIP

--- 0053-maximum-subarray.py
+++ 0053-maximum-subarray.py
@@ -16,29 +16,55 @@
 #         return sum_max
 
 
+class Sums:
+    def __init__(self, left, all, right):
+        self.left = left
+        self.all = all
+        self.right = right
+
+    def max(self):
+        return max(self.left, self.all, self.right)
+
+
 class Solution:
     @staticmethod
     def max_sub_array_rec(nums, index_left, index_right):
-        nums_effective = nums[index_left:index_right + 1]
+        """
+        :rtype: Sums
+        """
 
         length = index_right - index_left + 1
-
         if length == 1:
-            return nums[index_left]
+            return Sums(nums[index_left], 0, 0)
+
+        # TODO: remove
+        nums_effective = nums[index_left:index_right + 1]
 
         index_mid = index_left + length // 2
 
-        sum_max_left = Solution.max_sub_array_rec(nums, index_left, index_mid - 1)
-        sum_max_right = Solution.max_sub_array_rec(nums, index_mid, index_right)
+        sums_left = Solution.max_sub_array_rec(nums, index_left, index_mid - 1)
+        sums_right = Solution.max_sub_array_rec(nums, index_mid + 1, index_right)
 
-        if sum_max_left > 0 and sum_max_right > 0:
-            return sum_max_left + sum_max_right
-        elif sum_max_left <= 0 and sum_max_right <= 0:
-            return max(sum_max_left, sum_max_right)
-        elif sum_max_left <= 0 < sum_max_right:
-            return sum_max_right
-        elif sum_max_left > 0 >= sum_max_right:
-            return sum_max_left
+        sum_max_left = sums_left.max()
+        sum_max_right = sums_right.max()
+        sum_max_all = sums_left.all + nums[index_mid] + sums_right.all
+
+        return Sums(sum_max_left, sum_max_all, sum_max_right)
+        # if num_mid > 0:
+        #     sum_max_left += num_mid
+        #     sum_max_right += num_mid
+
+        # sum_max = max(sum_all, sum_max_left, sum_max_right)
+        # return sum_max
+
+        # if sum_max_left > 0 and sum_max_right > 0:
+        #     return sum_max_left + sum_max_right
+        # elif sum_max_left <= 0 and sum_max_right <= 0:
+        #     return max(sum_max_left, sum_max_right)
+        # elif sum_max_left <= 0 < sum_max_right:
+        #     return sum_max_right
+        # elif sum_max_left > 0 >= sum_max_right:
+        #     return sum_max_left
 
     def maxSubArray(self, nums):
         """
@@ -64,6 +90,9 @@ class Solution:
         if sum_running is not None:
             nums_condensed.append(sum_running)
 
+        if len(nums_condensed) == 1:
+            return nums_condensed[0]
+
         if nums_condensed[0] < 0:
             nums_condensed.pop(0)
 
@@ -71,7 +100,7 @@ class Solution:
             nums_condensed.pop()
 
         print(nums_condensed)
-        # return Solution.max_sub_array_rec(nums_condensed, 0, len(nums) - 1)
+        return Solution.max_sub_array_rec(nums_condensed, 0, len(nums_condensed) - 1).max()
 
 
 def main():

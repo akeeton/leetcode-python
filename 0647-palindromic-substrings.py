dc18257d07894da68b334d0880861dde9f9249a3
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-08-20 14:42:40 -0400

    0647 Accepted - runtime beats 77.54% of python3 submissions

--- 0647-palindromic-substrings.py
+++ 0647-palindromic-substrings.py
@@ -18,75 +18,51 @@ The input string length won't exceed 1000.
 
 
 class Solution:
-    @staticmethod
-    def do_stuff(s, left_bound, right_bound, palindromes):
-        """
-        :param s: str
-        :param left_bound: int
-        :param right_bound: int
-        :param palindromes: List[str]
-        :return: int
-        """
-
-        num_palindromes = 0
-
-        slice = s[left_bound:right_bound + 1]
-
-        length = right_bound - left_bound + 1
-        assert(length >= 1)
-
-        if length % 2 == 0:
-            # length is even so treat the middle two characters as a single middle character
-
-            middle_right = left_bound + length // 2
-            middle_left = middle_right - 1
-
-            if s[middle_left] != s[middle_right]:
-                return 0
-
-            left = middle_left
-            right = middle_right
-        else:
-            # length is odd so start at the middle character
-
-            middle = left_bound + length // 2
-            left = middle
-            right = middle
-
-        while left >= left_bound and right <= right_bound and right - left + 1 > 1:
-            if s[left] == s[right]:
-                palindromes.append(s[left:right + 1])
-                num_palindromes += 1
-            else:
-                break
-
-            left -= 1
-            right += 1
-
-        return num_palindromes
-
     def countSubstrings(self, s):
         """
         :type s: str
         :rtype: int
         """
 
-        if len(s) == 0 or len(s) == 1:
-            return True
+        if len(s) == 0:
+            return 0
+        elif len(s) == 1:
+            return 1
 
-        palindromes = []
+        # palindromes = []
         num_palindromes = 0
 
-        for left_bound in range(len(s)):
-            for right_bound in range(left_bound + 1, len(s)):
-                num_palindromes += Solution.do_stuff(s, left_bound, right_bound, palindromes)
+        for middle in range(len(s)):
+            left = middle
+            right = middle
 
-        for c in s:
-            palindromes.append(c)
+            while left >= 0 and right < len(s):
+                if s[left] == s[right]:
+                    # palindromes.append(s[left:right + 1])
+                    num_palindromes += 1
+                else:
+                    break
 
-        print(palindromes)
-        return len(palindromes)
-        # return num_palindromes + len(s) # len(s) for single-character palindromes
+                left -= 1
+                right += 1
+
+        for middle_left in range(len(s)):
+            left = middle_left
+            right = middle_left + 1
+
+            while left >= 0 and right < len(s):
+                if s[left] == s[right]:
+                    # palindromes.append(s[left:right + 1])
+                    num_palindromes += 1
+                else:
+                    break
+
+                left -= 1
+                right += 1
+
+        # print(palindromes)
+        # return len(palindromes)
+        return num_palindromes
 
 
 def main():

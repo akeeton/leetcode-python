63fcf1075ba2c0c1313663a9cb5fdc5e7ef6cc4b
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-08-21 14:32:33 -0400

    0283 Implement bubble sort

--- 0283-move-zeroes.py
+++ 0283-move-zeroes.py
@@ -14,15 +14,35 @@ Minimize the total number of operations.
 
 
 class Solution:
+    @staticmethod
+    def bubble_sort_zeroes(nums):
+        is_sorted = False
+
+        while not is_sorted:
+            is_sorted = True
+
+            for left in range(len(nums) - 1):
+                right = left + 1
+
+                if nums[left] == 0 and nums[right] != 0:
+                    nums[left], nums[right] = nums[right], nums[left]
+                    is_sorted = False
+
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
 
+        Solution.bubble_sort_zeroes(nums)
+
 
 def main():
-    pass
+    sol = Solution()
+
+    nums_a = [0, 1, 0, 3, 12]
+    sol.moveZeroes(nums_a)
+    print(nums_a)
 
 
 if __name__ == '__main__':

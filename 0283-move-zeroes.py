ec1f2165ccdb0d8763e90ed0ef15f9f6a954b3ab
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-08-21 14:41:47 -0400

    0283 Implement insertion sort - Time Limit Exceeded - 20/21 test cases passed

--- 0283-move-zeroes.py
+++ 0283-move-zeroes.py
@@ -28,13 +28,28 @@ class Solution:
                     nums[left], nums[right] = nums[right], nums[left]
                     is_sorted = False
 
+    @staticmethod
+    def insertion_sort_zeroes(nums):
+        for index_unsorted, num in enumerate(nums):
+            if index_unsorted == 0 or num == 0:
+                continue
+
+            for index_search_for_zero in range(index_unsorted):
+                if nums[index_search_for_zero] == 0:
+                    nums[index_search_for_zero] = num
+                    nums[index_unsorted] = 0
+
+                    break
+
+
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
 
-        Solution.bubble_sort_zeroes(nums)
+        # Solution.bubble_sort_zeroes(nums)
+        Solution.insertion_sort_zeroes(nums)
 
 
 def main():

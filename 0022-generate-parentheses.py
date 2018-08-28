94e79d54d73605126b4d67ac894d0fa034cfd4a7
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-08-28 16:40:42 -0400

    0022 Create skeleton

---
+++ 0022-generate-parentheses.py
@@ -0,0 +1,29 @@
+"""
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+"""
+
+
+class Solution:
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

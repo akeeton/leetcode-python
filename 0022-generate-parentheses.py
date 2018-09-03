754bc637f237add89a344603b867c8a7c96544f6
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-09-03 19:37:35 -0400

    0022 Accepted - runtime beats 31.03% of python3 submissions

--- 0022-generate-parentheses.py
+++ 0022-generate-parentheses.py
@@ -20,9 +20,39 @@ class Solution:
         :rtype: List[str]
         """
 
+        if n == 0:
+            return []
+
+        stack = [{'parens': ['('], 'open': 1, 'left': n - 1, 'right': n}]
+        solutions = []
+
+        while stack:
+            p = stack.pop()
+            assert(p['open'] + p['left'] == p['right'])
+
+            if p['left'] == 0 and p['right'] == 0:
+                solutions.append("".join(p['parens']))
+                continue
+
+            if p['open'] > 0:
+                parens_add_right = p['parens'] + [')']
+
+                p_add_right = {'parens': parens_add_right, 'open': p['open'] - 1, 'left': p['left'], 'right': p['right'] - 1}
+                stack.append(p_add_right)
+
+            if p['left'] > 0:
+                parens_add_left = p['parens'] + ['(']
+
+                p_add_left = {'parens': parens_add_left, 'open': p['open'] + 1, 'left': p['left'] - 1, 'right': p['right']}
+                stack.append(p_add_left)
+
+        return solutions
+
 
 def main():
-    pass
+    sol = Solution()
+
+    print(sol.generateParenthesis(3))
 
 
 if __name__ == '__main__':

2050982f47217be1a46938a5f188ae8f0b1c8f53
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-09-03 19:50:50 -0400

    0046 Create skeleton

---
+++ 0045-permutations.py
@@ -0,0 +1,34 @@
+"""
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+
+
+class Solution:
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+
+def main():
+    sol = Solution()
+
+    print(sol.permute([1, 2, 3]))
+
+
+if __name__ == '__main__':
+    main()

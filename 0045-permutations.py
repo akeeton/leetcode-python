9ce0117564561343d50a729b2359d28de8b0523b
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-09-03 20:56:34 -0400

    0046 Accepted - runtime beats 27.30% of python3 submissions

--- 0045-permutations.py
+++ 0045-permutations.py
@@ -17,11 +17,40 @@ Output:
 
 
 class Solution:
+    @staticmethod
+    def permute_using_set(nums):
+        """
+        :type nums: Set[int]
+        :rtype: List[List[int]]
+        """
+
+        if len(nums) == 0:
+            return []
+        elif len(nums) == 1:
+            return [[next(iter(nums))]]
+
+        nums_copy = nums.copy()
+        permutations = []
+
+        for num in nums:
+            nums_copy.remove(num)
+
+            partial_permutations = Solution.permute_using_set(nums_copy)
+
+            for partial_permutation in partial_permutations:
+                permutations.append([num] + partial_permutation)
+
+            nums_copy.add(num)
+
+        return permutations
+
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+
+        return Solution.permute_using_set(set(nums))
 
 
 def main():
